e19f247fe60fded0a0c993f683ee18a9aaa306ec
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-05-05 18:10:51 -0500

    Add CI

---
+++ SignSettings.psd1
@@ -0,0 +1,5 @@
+@{
+    FilePath = @('AnyPackage.Npm.psd1', 'AnyPackage.Npm.psm1')
+    TimeStampServer = 'http://timestamp.sectigo.com'
+    HashAlgorithm = 'SHA256'
+}
---
+++ test/Find-Package.Tests.ps1
@@ -0,0 +1,15 @@
+﻿#Requires -Modules AnyPackage.Npm
+
+Describe Find-Package {
+    Context 'with -Name parameter' {
+        It 'single name' {
+            Find-Package -Name cspell-dict-powershell |
+            Should -Not -BeNullOrEmpty
+        }
+
+        It 'multiple names' {
+            Find-Package -Name cspell-dict-powershell, posh-gulp |
+            Should -HaveCount 2
+        }
+    }
+}
