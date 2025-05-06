d65807d1b7edc489ded0c3373f905a3e26503a33
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2025-05-05 19:25:54 -0500

    Add CI (#10)

--- src/AnyPackage.Npm.psm1
+++ src/AnyPackage.Npm.psm1
@@ -1,4 +1,4 @@
-using module AnyPackage
+﻿using module AnyPackage
 using namespace AnyPackage.Provider
 
 [PackageProvider('Npm')]
@@ -14,10 +14,17 @@ class NpmProvider : PackageProvider, IGetPackage, IFindPackage {
             }
         }
     }
-    
+
     [void] GetPackage ([PackageRequest] $request) {
         $prefix = npm prefix --global
-        $globalNpmPackagePath = Join-Path -Path $prefix -ChildPath 'node_modules'
+
+        if ($global:PSEdition -eq 'Desktop' -or $global:IsWindows) {
+            $globalNpmPackagePath = Join-Path -Path $prefix -ChildPath 'node_modules'
+        }
+        else {
+            $globalNpmPackagePath = Join-Path -Path $prefix -ChildPath 'lib/node_modules'
+        }
+
         $request.WriteVerbose("Global installed packages directory: $globalNpmPackagePath")
 
         $packageJsonPath = Join-Path -Path $globalNpmPackagePath -ChildPath '*/package.json'
---
+++ test/Get-Package.Tests.ps1
@@ -0,0 +1,27 @@
+#Requires -Modules AnyPackage.Npm
+
+Describe Get-Package {
+    BeforeAll {
+        npm install cspell-dict-powershell -g
+        npm install posh-gulp -g
+    }
+
+    AfterAll {
+        npm uninstall cspell-dict-powershell -g
+        npm uninstall posh-gulp -g
+    }
+
+    Context 'with no parameters' {
+        It 'should return results' {
+            Get-Package |
+            Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context 'with -Name parameter' {
+        It 'should return cspell-dict-powershell' {
+            Get-Package -Name cspell-dict-powershell |
+            Should -Not -BeNullOrEmpty
+        }
+    }
+}
